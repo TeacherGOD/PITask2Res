aa71a47809d3b8b508e26998d3375eb8cadafce2
vpiatkov <vpiatkov@nocode.ru>
2024-11-10 15:08:32 +0500

    fix sh scripts

--- ПятковВК/task1.sh
+++ ПятковВК/task1.sh
@@ -1,41 +1,43 @@
-  174  git init
-  175  echo "В сторожке у каменных ворот въезда никого не было и дверь была отперта. /nДорожки сада уже заросли, и телята и лошади ходили по английскому парку." > story.txt
-  176  cat story.txt
-  177  git commit -m "Начало истории"
-  178  git add story.txt
-  179  git commit -m "Начало истории"
-  180  git checkout -b develop
-  181  git checkout -b feature/rain
-  182  echo "Шёл дождь" >> story.txt
-  183  git add story.txt
-  184  git commit -m "Добавлена часть о дожде"
-  185  git checkout develop
-  186  git merge feature/rain
-  187  git branch -d feature/rain
-  188  git checkout -b hotfix/natasha-death
-  189  echo "Наташа Ростова умерла, продолжение невозможно" >> story.txt
-  190  git add story.txt
-  191  git commit -m "Сообщение об ошибке: Наташа Ростова умерла, продолжение невозможно"
-  192  git checkout develop
-  193  git merge hotfix/natasha-death
-  194  git branch -d hotfix/natasha-death
-  195  git checkout -b hotfix/blank-ammo
-  196  echo "Зарядить пистолет холостыми." >> story.txt
-  197  git add story.txt
-  198  git commit -m "Проблема решена: холостые патроны"
-  199  git checkout develop
-  200  git merge hotfix/blank-ammo
-  201  git branch -d hotfix/blank-ammo
-  202  git checkout -b feature/remove-pillar
-  203  echo "Столб убран." >> story.txt
-  204  git add story.txt
-  205  git commit -m "Столб убран: обновление истории"
-  206  git checkout -b release/v0.1
-  207  git merge feature/remove-pillar
-  208  git branch -d feature/remove-pillar
-  209  git checkout main
-  210  git merge release/v0.1
-  211  git tag -a v0.1 -m "Релиз версии 0.1"
-  212  git branch -d release/v0.1
-  213  history > task1.sh
-  214  history 100 > task.sh
+#!/bin/bash
+
+git init
+echo "В сторожке у каменных ворот въезда никого не было и дверь была отперта. /nДорожки сада уже заросли, и телята и лошади ходили по английскому парку." > story.txt
+cat story.txt
+git commit -m "Начало истории"
+git add story.txt
+git commit -m "Начало истории"
+git checkout -b develop
+git checkout -b feature/rain
+echo "Шёл дождь" >> story.txt
+git add story.txt
+git commit -m "Добавлена часть о дожде"
+git checkout develop
+git merge feature/rain
+git branch -d feature/rain
+git checkout -b hotfix/natasha-death
+echo "Наташа Ростова умерла, продолжение невозможно" >> story.txt
+git add story.txt
+git commit -m "Сообщение об ошибке: Наташа Ростова умерла, продолжение невозможно"
+git checkout develop
+git merge hotfix/natasha-death
+git branch -d hotfix/natasha-death
+git checkout -b hotfix/blank-ammo
+echo "Зарядить пистолет холостыми." >> story.txt
+git add story.txt
+git commit -m "Проблема решена: холостые патроны"
+git checkout develop
+git merge hotfix/blank-ammo
+git branch -d hotfix/blank-ammo
+git checkout -b feature/remove-pillar
+echo "Столб убран." >> story.txt
+git add story.txt
+git commit -m "Столб убран: обновление истории"
+git checkout -b release/v0.1
+git merge feature/remove-pillar
+git branch -d feature/remove-pillar
+git checkout main
+git merge release/v0.1
+git tag -a v0.1 -m "Релиз версии 0.1"
+git branch -d release/v0.1
+history > task1.sh
+history 100 > task.sh
--- ПятковВК/task2.sh
+++ ПятковВК/task2.sh
@@ -1,36 +1,38 @@
-  224  git init
-  225  echo "В сторожке у каменных ворот въезда никого не было и дверь была отперта. \nДорожки сада уже заросли, и телята и лошади ходили по английскому парку." > story.txt
-  226  git add story.txt
-  227  git commit -m "Начало истории"
-  228  git checkout -b feature/rain
-  229  echo "Шёл дождь." >> story.txt
-  230  git add story.txt
-  231  git commit -m "Добавлена история о дожде"
-  232  git checkout main 
-  233  git merge feature/rain
-  234  git branch -d feature/rain
-  235  git checkout -b feature/natasha-death
-  236  echo "Наташа Ростова умерла, продолжение невозможно" >> story.txt
-  237  git add story.txt
-  238  git commit -m "Сообщение об ошибке: Наташа Ростова умерла, продолжение невозможно"
-  239  git checkout main
-  240  git merge feature/natasha-death
-  241  git branch -d feature/natasha-death
-  242  git checkout -b feature/blank-ammo
-  243  echo "Зарядить пистолет холостыми." >> story.txt
-  244  git add story.txt
-  245  git commit -m "Проблема решена: холостые патроны"
-  246  git checkout main
-  247  git merge feature/blank-ammo
-  248  git branch -d feature/blank-ammo
-  249  git checkout -b feature/remove-pillar
-  250  echo "Столб убран." >> story.txt
-  251  git add story.txt
-  252  git status
-  253  git commit -m "Столб убран: обновление истории"
-  254  git checkout main
-  255  git merge feature/remove-pillar
-  256  git branch -d feature/remove-pillar
-  257  git checkout release/v0.1
-  258  git checkout -b release/v0.1
-  259  git merge main
+#!/bin/bash
+
+git init
+echo "В сторожке у каменных ворот въезда никого не было и дверь была отперта. \nДорожки сада уже заросли, и телята и лошади ходили по английскому парку." > story.txt
+git add story.txt
+git commit -m "Начало истории"
+git checkout -b feature/rain
+echo "Шёл дождь." >> story.txt
+git add story.txt
+git commit -m "Добавлена история о дожде"
+git checkout main
+git merge feature/rain
+git branch -d feature/rain
+git checkout -b feature/natasha-death
+echo "Наташа Ростова умерла, продолжение невозможно" >> story.txt
+git add story.txt
+git commit -m "Сообщение об ошибке: Наташа Ростова умерла, продолжение невозможно"
+git checkout main
+git merge feature/natasha-death
+git branch -d feature/natasha-death
+git checkout -b feature/blank-ammo
+echo "Зарядить пистолет холостыми." >> story.txt
+git add story.txt
+git commit -m "Проблема решена: холостые патроны"
+git checkout main
+git merge feature/blank-ammo
+git branch -d feature/blank-ammo
+git checkout -b feature/remove-pillar
+echo "Столб убран." >> story.txt
+git add story.txt
+git status
+git commit -m "Столб убран: обновление истории"
+git checkout main
+git merge feature/remove-pillar
+git branch -d feature/remove-pillar
+git checkout release/v0.1
+git checkout -b release/v0.1
+git merge main
